9e0cdd1c3a1bcf7221aa5103601b473071ed0449
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2025-08-10 18:55:29 +0700

    feat: add clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+use clap::Command;
+
 pub fn hello() -> String {
     "Hello, world!".to_string()
 }
 
 pub fn run() {
+    let app = Command::new("zerv")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Dynamic Versioning CLI");
+
+    let _matches = app.get_matches();
     println!("{}", hello());
 }
 
